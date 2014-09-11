338ca5472177b943df7d41bb092f988e71d94576
aromanet <audrey.romanet42@gmail.com>
2014-09-11 10:56:56 +0200

    GIT - simplification de l'utilisation de DiffMerge

--- Git/git-diff-diffmerge-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# diff is called by git with 7 parameters:
-#  path old-file old-hex old-mode new-file new-hex new-mode
-diffmerge $2 $5 --title1="Old" --title2="New $1"
-#"$2" "$5" | cat
-
