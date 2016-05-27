e69a0c45f45dfd3e7728c2f2982136507afbe34a
aromanet <audrey.romanet42@gmail.com>
2016-05-27 09:48:38 +0200

    Rewriting xmobarr scripts to ruby
        Groovy scripts are very not performant as they launch a new JVM each
        time they are runned

--- bin/launchGroovy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export JAVA_HOME=/home/aromanet/dev/current_jdk
-SCRIPT_NAME=$1
-shift
-echo `/home/aromanet/dev/groovy/bin/groovy /home/aromanet/workspace/scripts/bin/$SCRIPT_NAME.groovy $*`
