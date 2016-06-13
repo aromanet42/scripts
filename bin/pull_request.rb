712160039370f1abe2130c7ec4e876f4874cdb3b
aromanet <audrey.romanet42@gmail.com>
2016-06-13 10:31:42 +0200

    [XMOBAR] List all open pull requests

--- bin/pull_request.rb
+++ bin/pull_request.rb
@@ -15,49 +15,53 @@ Net::HTTP.start(uri.host, uri.port,
     request = Net::HTTP::Get.new uri.request_uri
     request.basic_auth ENV['REPO_USERNAME'], ENV['REPO_TOKEN']
     response = http.request request
-    result = JSON.parse(response.body)
+    list_of_pr = JSON.parse(response.body)
 
-    if result.empty?
+    if list_of_pr.empty?
       exit
     end
 
-    monDernierMien = result[0]
+    size = list_of_pr.size.to_i - 1
 
-    pullRequestNumber = monDernierMien["number"]
-    title=" PR "+pullRequestNumber.to_s
+    (0..size).each { |i|
 
-    uri = URI(monDernierMien["labels_url"].gsub(/\{.*\}/, ''))
-    request = Net::HTTP::Get.new uri.request_uri
-    request.basic_auth ENV['REPO_USERNAME'], ENV['REPO_TOKEN']
-    response = http.request request
-    result = JSON.parse(response.body)
+      monDernierMien = list_of_pr[i.to_i]
+      pullRequestNumber = monDernierMien["number"]
+      title=" PR "+pullRequestNumber.to_s
 
-    if ! result.empty?
-      puts "<fc=##{result[0]["color"]}>#{title}</fc>"
-      exit
-    end
+      uri = URI(monDernierMien["labels_url"].gsub(/\{.*\}/, ''))
+      request = Net::HTTP::Get.new uri.request_uri
+      request.basic_auth ENV['REPO_USERNAME'], ENV['REPO_TOKEN']
+      response = http.request request
+      result = JSON.parse(response.body)
 
-    uri = URI(monDernierMien["pull_request"]["url"])
-    request = Net::HTTP::Get.new uri.request_uri
-    request.basic_auth ENV['REPO_USERNAME'], ENV['REPO_TOKEN']
-    response = http.request request
-    result = JSON.parse(response.body)
-     
-    uri = URI(result["_links"]["statuses"]["href"])
-    request = Net::HTTP::Get.new uri.request_uri
-    request.basic_auth ENV['REPO_USERNAME'], ENV['REPO_TOKEN']
-    response = http.request request
-    result = JSON.parse(response.body)
+      if ! result.empty?
+	puts "<fc=##{result[0]["color"]}>#{title}</fc>"
+      else
 
-    statuses = result.map { |it| it["state"] }.uniq
+	uri = URI(monDernierMien["pull_request"]["url"])
+	request = Net::HTTP::Get.new uri.request_uri
+	request.basic_auth ENV['REPO_USERNAME'], ENV['REPO_TOKEN']
+	response = http.request request
+	result = JSON.parse(response.body)
 
-    if statuses.include?("failure")
-      puts "<fc=#FF0000>#{title}</fc>"
-    elsif statuses.size == 1 && statuses[0] == "pending"
-      puts "<fc=#FFA500>#{title}</fc>"
-    elsif statuses.include?("success")
-      puts "<fc=#00FF00>#{title}</fc>"
-    end
-    puts " - "
+	uri = URI(result["_links"]["statuses"]["href"])
+	request = Net::HTTP::Get.new uri.request_uri
+	request.basic_auth ENV['REPO_USERNAME'], ENV['REPO_TOKEN']
+	response = http.request request
+	result = JSON.parse(response.body)
 
+	statuses = result.map { |it| it["state"] }.uniq
+
+	if statuses.include?("failure")
+	  puts "<fc=#FF0000>#{title}</fc>"
+	elsif statuses.size == 1 && statuses[0] == "pending"
+	  puts "<fc=#FFA500>#{title}</fc>"
+	elsif statuses.include?("success")
+	  puts "<fc=#00FF00>#{title}</fc>"
+	end
+      end
+      puts " - "
+
+    }
   end
