dd1580c470edf07712a785b96c34db7865774208
aromanet <audrey.romanet42@gmail.com>
2016-06-20 13:42:59 +0200

    [INSTALL] Add 3rd party repo for google-chrome installation

--- install.sh
+++ install.sh
@@ -71,6 +71,8 @@ sudo apt-get install arandr -y
 sudo ln -s $SCRIPTPATH/bin/detect-monitor-plugged.sh /usr/bin/detect-monitor-plugged.sh
 
 echo "CHROME..."
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get install google-chrome-stable -y
 
 # using libnotify notifications for chrome :
