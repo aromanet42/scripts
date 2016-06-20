79d6e560d4903079b55de59d9073415f285a9c21
aromanet <audrey.romanet42@gmail.com>
2016-06-20 13:48:22 +0200

    [INSTALL] update apt-get for Google Chrome installation

--- install.sh
+++ install.sh
@@ -90,7 +90,8 @@ sudo ln -s $SCRIPTPATH/bin/detect-monitor-plugged.sh /usr/bin/detect-monitor-plu
 
 echo "CHROME..."
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
 sudo apt-get install google-chrome-stable -y
 
 # using libnotify notifications for chrome :
