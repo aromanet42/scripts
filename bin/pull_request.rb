f0d2a47d8b4de47703d03c8b941a11e643b6ada3
aromanet <audrey.romanet42@gmail.com>
2017-01-10 15:43:01 +0100

    [Xmobar] Display all my PRs

--- bin/pull_request.rb
+++ bin/pull_request.rb
@@ -1,104 +1,146 @@
 #!/usr/bin/env ruby
 
-require "net/http"
-require "json"
-require "date"
+require 'net/http'
+require 'json'
+require 'date'
+require 'yaml'
+
+uri = URI("https://api.github.com/search/issues?q=author:#{ENV['REPO_USERNAME']}+is:open+type:pr")
 
 
-url = ENV['REPO_ENDPOINT'] + "/issues?creator="+ ENV['REPO_USERNAME']
+class GitRequests
+  TAG_FILENAME = '/tmp/pr.tag'
 
-uri = URI(url)
-Net::HTTP.start(uri.host, uri.port,
-  :use_ssl => uri.scheme == 'https',
-  :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+  def initialize(http)
+    @last_results = {}
+    @next_results = {}
+    @http = http
+  end
 
-    request = Net::HTTP::Get.new uri.request_uri
+  def fetch_response(uri, cache)
+    request_uri = uri.request_uri
+    request = Net::HTTP::Get.new request_uri
     request.basic_auth ENV['REPO_USERNAME'], ENV['REPO_TOKEN']
 
-    if File.exists?("/tmp/pr.tag")
-      tagfile = File.open("/tmp/pr.tag", "rb")
-      lastTag = tagfile.readline.strip
-      lastResult = tagfile.readline.strip
-      if !lastTag.to_s.empty?
-	request['If-None-Match'] = lastTag
-      end
+    last_etag = @last_results.fetch(request_uri, {})[:etag]
+    if last_etag && cache
+      request['If-None-Match'] = last_etag
     end
 
-    response = http.request request
+
+    response = @http.request request
 
     if response.is_a?(Net::HTTPNotModified)
-      puts lastResult
-      exit
+      return @last_results[request_uri][:response]
     end
 
-    responseLastTag = response['ETag']
-    tagfile = File.open("/tmp/pr.tag", "w")
-    tagfile.puts responseLastTag
-    tagfile.close
 
-    list_of_pr = JSON.parse(response.body)
-
-    if list_of_pr.empty?
-      exit
+    etag = response['ETag']
+    if etag
+      @next_results[request_uri] = {:etag => etag}
     end
 
-    #puts list_of_pr
-   # if !list_of_pr["message"].nil?
-   #   puts list_of_pr["message"]
-   #   exit
-   # end
+    result = JSON.parse(response.body)
 
-    size = list_of_pr.size.to_i - 1
+    if block_given?
+      result = yield(result)
+    end
 
-    to_print = ""
+    if cache
+      @next_results[request_uri][:response] = result
+    end
 
-    (0..size).each { |i|
+    result
+  end
 
-      monDernierMien = list_of_pr[i.to_i]
-      pullRequestNumber = monDernierMien["number"]
-      title=" PR "+pullRequestNumber.to_s
+  def get_repo_name(pr)
+    repo_url = pr['repository_url']
+    repo_url[(repo_url.rindex('/')+1)..-1]
+  end
 
-      uri = URI(monDernierMien["labels_url"].gsub(/\{.*\}/, ''))
-      request = Net::HTTP::Get.new uri.request_uri
-      request.basic_auth ENV['REPO_USERNAME'], ENV['REPO_TOKEN']
-      response = http.request request
-      result = JSON.parse(response.body)
+  def get_labels(pr)
+    uri = URI(pr['labels_url'].gsub(/\{.*\}/, ''))
+    fetch_response uri, true do |json|
+      json
+    end
+  end
 
-      if ! result.empty?
-	to_print += "<fc=##{result[0]["color"]}>#{title}</fc>"
+  def get_statuses(pr)
+    statuses_url = fetch_response URI(pr['pull_request']['url']), true do |json|
+      json['_links']['statuses']['href']
+    end
+
+    fetch_response URI(statuses_url), true do |json|
+      json.group_by{|it| it['context']}
+          .collect {|k, v| v.first['state']}
+    end
+  end
+
+  def load_last_results
+    if File.exists?(TAG_FILENAME)
+      @last_results = YAML.load_file(TAG_FILENAME)
+    end
+
+  end
+
+  def print_new_results
+
+    File.open(TAG_FILENAME, 'w') do |file|
+      file.write @next_results.to_yaml
+    end
+  end
+
+  def print_prs(uri)
+
+    load_last_results
+
+    list_of_pr = fetch_response uri, false do |json|
+      json['items']
+    end
+
+    output=''
+
+    list_of_pr.each { |pr|
+
+      pull_request_number = pr['number']
+
+      pr_labels = get_labels pr
+
+      repo_name = get_repo_name pr
+
+      title=" [#{repo_name}] PR #{pull_request_number.to_s}"
+
+      if !pr_labels.empty?
+        output += "<fc=##{pr_labels[0]['color']}>#{title}</fc>"
       else
 
-	uri = URI(monDernierMien["pull_request"]["url"])
-	request = Net::HTTP::Get.new uri.request_uri
-	request.basic_auth ENV['REPO_USERNAME'], ENV['REPO_TOKEN']
-	response = http.request request
-	result = JSON.parse(response.body)
+        statuses = get_statuses pr
 
-	uri = URI(result["_links"]["statuses"]["href"])
-	request = Net::HTTP::Get.new uri.request_uri
-	request.basic_auth ENV['REPO_USERNAME'], ENV['REPO_TOKEN']
-	response = http.request request
-	result = JSON.parse(response.body)
-
-	statuses = result.map { |it| it["state"] }.uniq
-
-	if statuses.include?("failure")
-	  to_print += "<fc=#FF0000>#{title}</fc>"
-	elsif statuses.size == 1 && statuses[0] == "pending"
-	  to_print += "<fc=#FFA500>#{title}</fc>"
-	elsif statuses.include?("success")
-	  to_print += "<fc=#00FF00>#{title}</fc>"
-	end
+        if statuses.include?('pending')
+          output += "<fc=#FFA500>#{title}</fc>"
+        elsif statuses.include?('failure')
+          output += "<fc=#FF0000>#{title}</fc>"
+        elsif statuses.include?('success')
+          output += "<fc=#00FF00>#{title}</fc>"
+        end
       end
-      to_print += " - "
+      output += ' - '
 
     }
 
+    # Writing result to file. If next API call is 'Not Modified' then we'll use this
+    print_new_results
 
-    puts to_print
-
-    tagfile = File.open("/tmp/pr.tag", "a")
-    tagfile.puts to_print
-    tagfile.close
+    output
 
   end
+
+end
+
+Net::HTTP.start(uri.host, uri.port,
+                :use_ssl => uri.scheme == 'https',
+                :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+
+  puts GitRequests.new(http).print_prs(uri)
+end
+
