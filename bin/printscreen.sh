447648d06a588685afbdef71fe360634a88b757e
aromanet <audrey.romanet42@gmail.com>
2017-09-12 15:00:31 +0200

    [Xmobar] Improve print screen

--- bin/printscreen.sh
+++ bin/printscreen.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# creating pipe if does not exist yet
+mkfifo /tmp/notif 2> /dev/null
+
+echo "Select area to capture..." > /tmp/notif
+
 sleep 0.2 # needed to avoid "couldn't grab keyboard:Resource temporarily unavailable" error
 
 mkdir -p /tmp/screenshots
@@ -8,5 +13,7 @@ now=$(date +"%Y%m%d-%H%M%S")
 filename="/tmp/screenshots/screenshot-${now}.png"
 scrot -s $filename
 
-notify-send "capture saved in $filename"
+echo "<action=kolourpaint $filename>capture saved in $filename</action>" > /tmp/notif
+sleep 8
+echo " " > /tmp/notif
 
--- bin/printwindow.sh
+++ bin/printwindow.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+# creating pipe if does not exist yet
+mkfifo /tmp/notif 2> /dev/null
+
+echo "Will print whole window in 3 seconds..." > /tmp/notif
+sleep 1
+echo "Will print whole window in 2 seconds..." > /tmp/notif
+sleep 1
+echo "Will print whole window in 1 second..." > /tmp/notif
+sleep 1
+
 mkdir -p /tmp/screenshots
 
 now=$(date +"%Y%m%d-%H%M%S")
 filename="/tmp/screenshots/screenshot-${now}.png"
 scrot $filename
-kolourpaint $filename
 
-
-notify-send "capture saved in $filename"
+echo "<action=kolourpaint $filename>capture saved in $filename</action>" > /tmp/notif
+sleep 8
+echo " " > /tmp/notif
 
