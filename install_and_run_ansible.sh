fe0c4984eac579084419d6b2e1b2df4fbd9a4eaa
Audrey Romanet <aromanet42@users.noreply.github.com>
2018-11-16 13:14:20 +0100

    Add Git installation

--- install_and_run_ansible.sh
+++ install_and_run_ansible.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-sudo apt-get install python-pip -y
+sudo apt-get install python-pip git -y
 pip install --upgrade pip
 
 sudo pip install ansible
 
 mkdir -p ~/workspace/scripts
+
+
+ssh-keygen -o
+
+echo "Add your public SSH key in Github Settings: https://github.com/settings/keys"
+
+read -p "Press enter when it is done"
 
 ansible-pull -d ~/workspace/scripts -o -U git@github.com:aromanet42/scripts.git -K
 
