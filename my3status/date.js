282b8c5e43a2338b69117a2aa792545b8efd4988
aromanet <audrey.romanet42@gmail.com>
2018-12-07 17:28:14 +0100

    [I3bar] Do not display seconds

--- my3status/date.js
+++ my3status/date.js
@@ -1,7 +1,21 @@
+function getZeroNumber(n) {
+    if (n < 10) {
+        return '0' + n;
+    }
+
+    return n;
+}
+
+
 module.exports = () => {
+    const date = new Date();
+
+    const formattedDate = `${date.getFullYear()}-${getZeroNumber(date.getMonth())}-${getZeroNumber(date.getDate())}`;
+    const formattedTime = `${date.getHours()}:${date.getMinutes()}`;
+
     return {
         name: 'date',
-        full_text: new Date().toLocaleString(),
+        full_text: `${formattedDate} ${formattedTime}`,
     };
 };
 
