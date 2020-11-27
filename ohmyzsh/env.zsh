d566ed43efd5d5d5f0eafeda51a3b4034df2dcb7
aromanet <audrey.romanet42@gmail.com>
2020-11-27 09:41:18 +0100

    Add GO to PATH

--- ohmyzsh/env.zsh
+++ ohmyzsh/env.zsh
@@ -9,7 +9,7 @@ export HISTTIMEFORMAT='%F %T ' # show history with datetime
 export MAVEN_HOME=$DEV/apache-maven
     
 
-export PATH="$PATH:$MAVEN_HOME/bin:$SHARED_TOOLS/scripts:~/.yarn.bin"
+export PATH="$PATH:$MAVEN_HOME/bin:$SHARED_TOOLS/scripts:~/.yarn.bin:/usr/local/go/bin"
   
 
 
