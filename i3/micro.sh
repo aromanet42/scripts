c18802d8924978265216fedfc41127f16e3102e9
aromanet <audrey.romanet42@gmail.com>
2024-01-02 18:11:38 +0100

    [I3] Remove custom i3 bar and migrate to I3Blocks
    I started to have a lot of issues with my custom Java I3 bar:
    - github PRs often exceeded quotas
    - clicking on github PRs was not responsive: it could take up to 30
    second to open a browser tab after clicking
    - date started to freeze
    
    the new version use I3 blocks:
    - no more custom scripts
    - volume can be increased/decreased with mouse scroll
    - date is exact
    - no more github PR listing :'(

--- i3/micro.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# handle microphone ('Capture'), whatever its name
-
-micAmixer=$(amixer | grep -B1 cvolume)
-micName=$(expr "$micAmixer" : "Simple mixer control '\(.*\)'")
-
-amixer set $micName toggle
--- my3status-java/src/main/java/fr/aromanet42/my3status/bar/items/github/PRCheck.java
+++
@@ -1,7 +0,0 @@
-package fr.aromanet42.my3status.bar.items.github;
-
-public record PRCheck(
-        String name,
-        PRCheckStatus status
-) {
-}
