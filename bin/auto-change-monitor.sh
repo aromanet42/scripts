188e365020c4539d2a17745fa3b3ccebd3385a7d
aromanet <audrey.romanet42@gmail.com>
2024-11-28 11:07:15 +0100

    Remove outdated scripts and resources

--- bin/auto-change-monitor.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# launching autorandr as 'aromanet' user
-# because xrandr can not be runned as root (udev is launched as root)
-su -c "DISPLAY=:0 autorandr -c" "aromanet"
--- bin/checkupdates.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get autoremove
-sudo apt-get autoclean
-
